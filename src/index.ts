f617956a2a949e51c144061cbb094682b86927e0
Tim Haywood <tim@haywood.org>
2021-03-30 09:55:45 +1100

    get it working with numbers

--- src/index.ts
+++ src/index.ts
@@ -1,31 +1,76 @@
 // Importing object bases (CompBase, LayerBase, PropertyBase)
 // TypeScript types (Layer, Comp, Value, Color etc)
 // and global functions from 'expression-globals-typescript'
-import { Comp, Layer } from 'expression-globals-typescript';
-import { fromOtherFile } from './otherFile';
+import { Comp, Layer, Property } from 'expression-globals-typescript';
 
 // Creating a new composition object from CompBase
 const thisComp = new Comp();
 const thisLayer = new Layer();
+const thisProperty = new Property<number>(0);
 
-// Using the expression types in a function
-function getLayerDuration(layerName: string) {
-  const layer: Layer = thisComp.layer(layerName);
-  return layer.outPoint - layer.inPoint;
+interface SpringProps {
+  mass: number;
+  stiffness: number;
+  damping: number;
+  initialVelocity: number;
 }
 
-// Using expressions global functions
-function remap(value: number) {
-  return thisLayer.linear(value, 0, 10, 0, 1);
-}
+function spring(
+  { mass = 1, stiffness = 100, damping = 12, initialVelocity = 0 }: SpringProps,
+  time = thisLayer.time
+) {
+  if (thisProperty.numKeys < 1) {
+    throw new Error('Property must have keyframes to use eSpring');
+  }
+  if (time < thisProperty.key(2).time) {
+    return thisProperty.key(1).value;
+  }
+  const w0: number = Math.sqrt(stiffness / mass);
+  const zeta: number = damping / (2 * Math.sqrt(stiffness * mass));
+  const a: number = 1;
+  const isUnderDamped: boolean = zeta < 1;
 
-function welcome(name: string): string {
-  return `Welcome ${name}!`;
-}
+  const wd: number = isUnderDamped ? w0 * Math.sqrt(1 - zeta * zeta) : 0;
+  const b: number = isUnderDamped
+    ? (zeta * w0 + -initialVelocity) / wd
+    : -initialVelocity + w0;
 
-const someValue: number = 2;
+  function getVal(progress: number) {
+    if (isUnderDamped) {
+      return (
+        1 -
+        Math.exp(-progress * zeta * w0) *
+          (a * Math.cos(wd * progress) + b * Math.sin(wd * progress))
+      );
+    } else {
+      return 1 - (a + b * progress) * Math.exp(-progress * w0);
+    }
+  }
+
+  const currentKeyIndex = getLastKeyIndex();
+  const prevKey = thisProperty.key(currentKeyIndex - 1);
+  const currentKey = thisProperty.key(currentKeyIndex);
+
+  return (
+    prevKey.value +
+    (currentKey.value - prevKey.value) * getVal(time - currentKey.time)
+  );
+
+  function getLastKeyIndex() {
+    // Set curKey to the previous keyframe
+    if (thisLayer.time < thisProperty.key(1).time) {
+      return 1;
+    }
+    const curKey = thisProperty.nearestKey(time).index;
+    if (thisProperty.key(curKey).time > time) {
+      return curKey - 1;
+    }
+
+    return curKey;
+  }
+}
 
 const version: string = '_npmVersion';
 
 // Export values to appear in jsx files
-export { getLayerDuration, remap, welcome, someValue, version, fromOtherFile };
+export { version, spring };
